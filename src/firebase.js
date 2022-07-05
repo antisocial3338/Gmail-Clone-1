1b2e3cbb524189af58abac3212d3def4a02d704d
antisocial3338 <antisocial3338@gmail.com>
2022-07-05 19:21:51 +0630

    hi

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCodlKGpQIbS0CkUENt6UiTvPooeC99hZI",
-  authDomain: "gmaiil-clone-v1.firebaseapp.com",
-  projectId: "gmaiil-clone-v1",
-  storageBucket: "gmaiil-clone-v1.appspot.com",
-  messagingSenderId: "1083004158308",
-  appId: "1:1083004158308:web:c20527bd67bea90497ba71",
+  apiKey: "AIzaSyATTcoDaK9Zgb0UlacaxhitGtihBMzQCI0",
+  authDomain: "signal-social.firebaseapp.com",
+  projectId: "signal-social",
+  storageBucket: "signal-social.appspot.com",
+  messagingSenderId: "407940045832",
+  appId: "1:407940045832:web:60a3e65bbcc45a031485bd",
   measurementId: "G-SB24KMVB5J",
 };
 
